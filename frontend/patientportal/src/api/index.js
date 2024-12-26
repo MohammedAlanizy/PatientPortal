959760854ede3c006ebbff35cb2d8ce51d2ec301
MohammedAlanizy <alanizym@msu.edu>
2024-12-26 09:59:17 -0500

    For some reason if the api has parms then it needs a (/) at the end. Weird

--- frontend/patientportal/src/api/index.js
+++ frontend/patientportal/src/api/index.js
@@ -40,25 +40,25 @@ const filterEmptyValues = (obj) => {
 };
   
 export const requestsApi = {
-    getRequests: (params) => api.get('/requests', { params: filterEmptyValues(params) }),
-    getRequest: (id) => api.get(`/requests/${id}`),
-    createRequest: (data) => api.post('/requests', filterEmptyValues(data)),
-    updateRequest: (id, data) => api.put(`/requests/${id}`, filterEmptyValues(data)),
+    getRequests: (params) => api.get('/requests/', { params: filterEmptyValues(params) }),
+    getRequest: (id) => api.get(`/requests/${id}/`),
+    createRequest: (data) => api.post('/requests/', filterEmptyValues(data)),
+    updateRequest: (id, data) => api.put(`/requests/${id}/`, filterEmptyValues(data)),
     getStats: () => api.get('/requests/stats'),
   };
   
   export const assigneesApi = {
-    getAssignees: (params) => api.get('/assignees', { params: filterEmptyValues(params) }),
-    createAssignee: (data) => api.post('/assignees', filterEmptyValues(data)),
-    updateAssignee: (id, data) => api.put(`/assignees/${id}`, filterEmptyValues(data)),
-    deleteAssignee: (id) => api.delete(`/assignees/${id}`),
+    getAssignees: (params) => api.get('/assignees/', { params: filterEmptyValues(params) }),
+    createAssignee: (data) => api.post('/assignees/', filterEmptyValues(data)),
+    updateAssignee: (id, data) => api.put(`/assignees/${id}/`, filterEmptyValues(data)),
+    deleteAssignee: (id) => api.delete(`/assignees/${id}/`),
     getStats: () => api.get('/assignees/stats'),
   };
   
   export const usersApi = {
     getMe: () => api.get('/users/me'),
-    createUser: (data) => api.post('/users', filterEmptyValues(data)),
-    getUsers: (params) => api.get('/users', { params: filterEmptyValues(params) }),
-    deleteUser: (id) => api.delete(`/users/${id}`),
+    createUser: (data) => api.post('/users/', filterEmptyValues(data)),
+    getUsers: (params) => api.get('/users/', { params: filterEmptyValues(params) }),
+    deleteUser: (id) => api.delete(`/users/${id}/`),
   };
   
