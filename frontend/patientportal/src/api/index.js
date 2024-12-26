59a6ac86d7a6eca24b6be74d1895b22f5f5d509f
MohammedAlanizy <alanizym@msu.edu>
2024-12-26 10:02:28 -0500

    Something weird is happening

--- frontend/patientportal/src/api/index.js
+++ frontend/patientportal/src/api/index.js
@@ -41,17 +41,17 @@ const filterEmptyValues = (obj) => {
   
 export const requestsApi = {
     getRequests: (params) => api.get('/requests/', { params: filterEmptyValues(params) }),
-    getRequest: (id) => api.get(`/requests/${id}/`),
+    getRequest: (id) => api.get(`/requests/${id}`),
     createRequest: (data) => api.post('/requests/', filterEmptyValues(data)),
-    updateRequest: (id, data) => api.put(`/requests/${id}/`, filterEmptyValues(data)),
+    updateRequest: (id, data) => api.put(`/requests/${id}`, filterEmptyValues(data)),
     getStats: () => api.get('/requests/stats'),
   };
   
   export const assigneesApi = {
     getAssignees: (params) => api.get('/assignees/', { params: filterEmptyValues(params) }),
     createAssignee: (data) => api.post('/assignees/', filterEmptyValues(data)),
-    updateAssignee: (id, data) => api.put(`/assignees/${id}/`, filterEmptyValues(data)),
-    deleteAssignee: (id) => api.delete(`/assignees/${id}/`),
+    updateAssignee: (id, data) => api.put(`/assignees/${id}`, filterEmptyValues(data)),
+    deleteAssignee: (id) => api.delete(`/assignees/${id}`),
     getStats: () => api.get('/assignees/stats'),
   };
   
@@ -59,6 +59,6 @@ export const requestsApi = {
     getMe: () => api.get('/users/me'),
     createUser: (data) => api.post('/users/', filterEmptyValues(data)),
     getUsers: (params) => api.get('/users/', { params: filterEmptyValues(params) }),
-    deleteUser: (id) => api.delete(`/users/${id}/`),
+    deleteUser: (id) => api.delete(`/users/${id}`),
   };
   
